f4a3dd2f066beb5a9348224de6a2af937eaacb9e
philcronin <philcronin20@gmail.com>
2020-10-19 16:04:03 -0400

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+let name = "Mitch Cuckovich"
+let age = "25"
+let birthday = "January 24"
+const detroitGC ="true"
+let lifeEvents = ["I was born in Troy, Michigan.", "I went to Hope College", "I went to Junior Olympics when I was 10 years old. I placed 14th in the nation in the 800 meter event.", "I'm a graduate of the front-end bootcamp."}
+
